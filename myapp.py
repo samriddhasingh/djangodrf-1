34565ac8a1bf1a1960f53a31792962888394c103
Samriddha Singh <59139917+samriddhasingh@users.noreply.github.com>
2022-11-30 20:16:06 +0545

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,17 @@
+import requests
+import json
+URL='http://127.0.0.1:8000/studentcreate/'
+
+data={
+    'name': 'Samriddha',
+    'roll':101,
+    'city':'Bhemdatt'
+}
+
+json_data=json.dumps(data)
+
+r=requests.post(url=URL,data=json_data)
+
+data=r.json()
+print(data)
+
