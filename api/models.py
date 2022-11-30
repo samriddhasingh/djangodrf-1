5369b2f9a6b9f79cbf8b8b9b87d93b3241024492
Samriddha Singh <59139917+samriddhasingh@users.noreply.github.com>
2022-11-30 20:20:37 +0545

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Student(models.Model):
+    name=models.CharField(max_length=1000)
+    roll=models.IntegerField()
+    city=models.CharField(max_length=1000)
+
+
